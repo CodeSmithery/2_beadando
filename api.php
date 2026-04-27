62921412a937e0d14e6db88c8e6589f76f4a17e3
CodeSmithery <kovacs.szabolcs.p@gmail.com>
2026-04-27 20:03:30 +0200

    Initial commit with base files to be used.

---
+++ api.php
@@ -0,0 +1,45 @@
+<?php
+require "db.php";
+$action = $_POST['action'];
+switch ($action) {
+    case 'read':  
+        try {
+            $stmt = $pdo->query("SELECT * FROM users");
+            $readData=$stmt->fetchAll();
+            echo json_encode(['status' => 'Read success!', "readData"=>$readData]);
+        }
+        catch(PDOException $e) {
+          echo json_encode(['status' => 'Read error!']);
+        }
+        break;
+    case 'create':
+        try {
+            $stmt = $pdo->prepare("INSERT INTO users (name, email) VALUES (?, ?)");
+            $stmt->execute([$_POST["name"], $_POST["email"]]);
+            echo json_encode(['status' => 'Create success!']);
+        }
+        catch(PDOException $e) {
+          echo json_encode(['status' => 'Create error!']);
+        }
+        break;
+    case 'update':
+        try {
+            $stmt = $pdo->prepare("UPDATE users SET name=?, email=? WHERE id=?");
+            $stmt->execute([$_POST["name"], $_POST["email"], $_POST["id"]]);
+            echo json_encode(['status' => 'Update success!']);
+        }
+        catch(PDOException $e) {
+          echo json_encode(['status' => 'Update error!']);
+        }
+        break;
+    case 'delete':
+        try {
+            $stmt = $pdo->prepare("DELETE FROM users WHERE id=?");
+            $stmt->execute([$_POST["id"]]);
+            echo json_encode(['status' => 'Delete success!']);
+        }
+        catch(PDOException $e) {
+          echo json_encode(['status' => 'Delete error!']);
+        }
+        break;
+}
