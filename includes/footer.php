5845da8cf14c9c9d8c803bec0e774729dcb02a37
Figura Kristóf <figura.kristof@gmail.com>
2026-04-28 17:38:28 +0200

    Add header and footer structure to main index page

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,26 @@
 /*
     A lábléc, ami minden oldalon megjelenik.
 */
+
+<hr>
+
+<footer>
+    <div>
+        <a href="https://github.com/CodeSmithery" target="_blank">
+            Kovács Szabolcs - EBKILY
+        </a>
+        |
+        <a href="https://drive.google.com/drive/folders/12lDIxariOadDkGPaEssksPnL63lzYCbg" target="_blank">
+            <abbr title="2-13-Tankönyvrendelés-4 táblás">
+                Felhasznált adatbázis
+            </abbr>
+        </a>
+        |
+        <a href="https://github.com/FKristof97" target="_blank">
+            Figura Kristóf - IDNNBT
+        </a>
+    </div>
+</footer>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,3 +2,38 @@
     A weboldal kezdőlapja. Itt jelenik meg a menü, a fő tartalom, a videók és a téma bemutatása. 
     A header.php és footer.php fájlokkal együtt épül fel.
 */
+
+<?php
+session_start();
+
+$page = $_GET['page'] ?? 'home';
+
+include "includes/header.php";
+
+if ($page == "home") {
+    echo "<h2>Főoldal</h2>";
+}
+elseif ($page == "crud") {
+    include "crud.php";
+}
+elseif ($page == "login") {
+    include "login.php";
+}
+elseif ($page == "register") {
+    include "register.php";
+}
+elseif ($page == "logout") {
+    include "logout.php";
+}
+elseif ($page == "pictures") {
+    include "pictures.php";
+}
+elseif ($page == "connection") {
+    include "connection.php";
+}
+else {
+    echo "404 - Nincs ilyen oldal";
+}
+
+include "includes/footer.php";
+?>
