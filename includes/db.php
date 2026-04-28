11c6bc7b126d0621f7177c7440636161d618df86
CodeSmithery <kovacs.szabolcs.p@gmail.com>
2026-04-28 19:36:55 +0200

    Remove unused files and add home page with video content

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,19 @@
-/*
-    Az adatbázis kapcsolatot létrehozó fájl (PDO).
-    Ezt az api.php és a login/regisztrációs oldalak használják.
-*/
-
 <?php
 $host = "localhost";
 $db   = "varosadatbazis";
-$user = "root";
+$user = "varosadatbazis";
 $pass = "NJEBeadando2lab";
+
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=UTF8",$user,$pass,
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=UTF8",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
 } catch (PDOException $e) {
-    die("Database connection failed");
+    die("Adatbázis hiba: " . $e->getMessage());
 }
--- public/scripts/main.js
+++
@@ -1,6 +0,0 @@
-/* 
-    - az oldal általános működéséért felel
-    - kezeli a menü működését (pl. mobil nézetben)
-    - betölti vagy frissíti az oldal bizonyos elemeit
-    - tartalmazhat kisebb, közös JavaScript funkciókat, amiket több oldal is használ
-*/
